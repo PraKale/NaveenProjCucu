c47adfe42d6364b7507c2ed082c49e3abdb675c4
rajkumaringale <rajkumaringale77@gmail.com>
2022-05-02 14:09:48 +0530

    This is raj project

---
+++ src/test/java/gitPractice/Romeo.java
@@ -0,0 +1,12 @@
+package gitPractice;
+
+public class Romeo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	 System.out.println("Hello guys whats up");
+		
+	}
+
+}
