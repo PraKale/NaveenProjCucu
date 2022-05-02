2068353c692463da3c3a5ca6bf36ca6dd6fcc7ed
TKKumbhar <tushkumbhar01@gmail.com>
2022-05-02 14:08:56 +0530

    This is Tushar's first comment

---
+++ src/test/java/gitPractice/Temp.java
@@ -0,0 +1,11 @@
+package gitPractice;
+
+public class Temp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello guys whats up");
+
+	}
+
+}
