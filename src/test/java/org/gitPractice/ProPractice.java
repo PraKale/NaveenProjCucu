73a27e4ed15f5d7361bb05b50bed7fe9c6b0136a
GaneshKoravi <ganeshkoravi95@gmail.com>
2022-05-02 14:16:08 +0530

    Hi jaanu

--- src/test/java/org/gitPractice/ProPractice.java
+++ src/test/java/org/gitPractice/ProPractice.java
@@ -4,7 +4,7 @@ public class ProPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello gous its Lunch Time");
+		System.out.println("Hello guys its Lunch Time");
 	}
 
 }
