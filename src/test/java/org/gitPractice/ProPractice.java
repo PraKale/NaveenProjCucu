01f9bb44e210c28c8cbb5b29a308fb5f8bd64624
GaneshKoravi <ganeshkoravi95@gmail.com>
2022-05-02 14:08:44 +0530

    This is first Program

---
+++ src/test/java/org/gitPractice/ProPractice.java
@@ -0,0 +1,10 @@
+package org.gitPractice;
+
+public class ProPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello gous its Lunch Time");
+	}
+
+}
